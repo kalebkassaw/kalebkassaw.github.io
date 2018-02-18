409593f3c2474a77d380c59f9686949231ca5624
kalebkassaw <kkassaw22@gmail.com>
2018-02-17 23:44:43 -0600

    new feature!
    incomplete system of equations solver. will be completed in future
    iterations

---
+++ matrix.js
@@ -0,0 +1,43 @@
+let row0 = [0, 0, 0];
+let row1 = [0, 0, 0];
+
+row0[0] = document.getElementById("a00");
+row0[1] = document.getElementById("a01");
+row1[0] = document.getElementById("a10");
+row1[1] = document.getElementById("a11");
+
+row0[2] = document.getElementById("a02");
+row1[2] = document.getElementById("a12");
+
+if (row0[0] != 1) {
+    row0[1] = row0[1] / row0[0];
+    row0[2] = row0[2] / row0[0];
+    // Have to do this one last!!!!
+    row0[0] = row0[0] / row0[0];
+
+    console.log('after first op:');
+    console.log(row0);
+    console.log(row1);
+}
+
+if (row1[0] != 0) {
+    row1[1] = row1[1] - row1[0]*row0[1];
+    row1[2] = row1[2] - row1[0]*row0[2];
+    // Again, must be last.
+    row1[0] = row1[0] - row1[0]*row0[0];
+
+    console.log('after second op:');
+    console.log(row0);
+    console.log(row1);
+}
+
+if (row0[1] != 0) {
+    row0[0] = row0[0] - row0[1]*row1[0];
+    row0[2] = row0[2] - row0[1]*row1[2];
+    // MUST BE F&%$ING LAST but with a twist (lol)
+    row0[1] = row0[1] - row0[1]*row1[1];
+
+    console.log('after third op:');
+    console.log(row0);
+    console.log(row1);
+}
