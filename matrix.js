e300bd49ab58a8ff93c18cb1400136024833dc83
kalebkassaw <kkassaw22@gmail.com>
2018-02-18 22:18:04 -0600

    matrix near-completion!

--- matrix.js
+++ matrix.js
@@ -2,13 +2,13 @@ function solve(){
 let row0 = [0, 0, 0];
 let row1 = [0, 0, 0];
 
-row0[0] = Number(document.getElementById("a00"));
-row0[1] = Number(document.getElementById("a01"));
-row1[0] = Number(document.getElementById("a10"));
-row1[1] = Number(document.getElementById("a11"));
+row0[0] = Number(document.getElementById("a00").value);
+row0[1] = Number(document.getElementById("a01").value);
+row1[0] = Number(document.getElementById("a10").value);
+row1[1] = Number(document.getElementById("a11").value);
 
-row0[2] = Number(document.getElementById("a02"));
-row1[2] = Number(document.getElementById("a12"));
+row0[2] = Number(document.getElementById("a02").value);
+row1[2] = Number(document.getElementById("a12").value);
 
 console.log(row0);
 console.log(row1);
@@ -35,15 +35,28 @@ if (row1[0] != 0) {
     console.log(row1);
 }
 
+if (row1[1] != 1) {
+    row1[0] = row1[0] / row1[1];
+    row1[2] = row1[2] / row1[1];
+    // L A S T
+    row1[1] = row1[1] / row1[1];
+
+    console.log('after third op:');
+    console.log(row0);
+    console.log(row1);
+}
+
 if (row0[1] != 0) {
     row0[0] = row0[0] - row0[1]*row1[0];
     row0[2] = row0[2] - row0[1]*row1[2];
     // MUST BE F&%$ING LAST but with a twist (lol)
     row0[1] = row0[1] - row0[1]*row1[1];
 
-    console.log('after third op:');
+    console.log('after fourth op:');
     console.log(row0);
     console.log(row1);
 }
+
+document.write(`x0 = ${row0[2]} & x1 = ${row1[2]}`);
 return 0;
 }
